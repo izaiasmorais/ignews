75d640e08db97aca5e10299fd5820f228a6e2690
Izaias Lima <53953937+IzaiasMorais@users.noreply.github.com>
2022-07-06 15:55:32 -0300

    update-6

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -13,7 +13,7 @@ export function SignInButton() {
       onClick={() => signOut()}
     >
       <FaGithub color="#04d361" />
-      Izaías Lima
+      {session!.user!.name!}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "/api",
+  baseURL: "https://igblog.vercel.app/api",
 });
